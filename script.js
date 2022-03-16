2b341dcf2e5ce94892f000f6da4e1c53352935e8
LoanPerchirin <loan.perchirin@gmail.com>
2022-03-16 09:13:33 +0100

    loan1

---
+++ script.js
@@ -0,0 +1,14 @@
+let hamburgerMenuState='close';
+
+function doClickHamburger(){
+    console.log('On est là!');
+    console.log(hamburgerMenuState);
+    if(hamburgerMenuState=='close'){
+        document.getElementById("menuHamburger").style.display = "flex";
+        hamburgerMenuState='open';
+    }else{
+        document.getElementById("menuHamburger").style.display = "none";
+        hamburgerMenuState='close';
+    }
+}
+
