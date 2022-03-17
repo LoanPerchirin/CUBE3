a1f826d19caae7ad9ddeecfe4c67e3c3446baa96
Sayki <wayan.savary@gmail.com>
2022-03-17 11:59:51 +0100

    page index
    page index fini avec php et les carousel fonctionnel
    
    ajout lien
    
    ajout du lien vers l'accueil quand on click sur le logo

---
+++ index/script.js
@@ -0,0 +1,56 @@
+let hamburgerMenuState='close';
+
+function doClickHamburger(){
+    console.log(hamburgerMenuState);
+    if(hamburgerMenuState=='close'){
+        document.getElementById("menuHamburger").style.display = "flex";
+        hamburgerMenuState='open';
+    }else{
+        document.getElementById("menuHamburger").style.display = "none";
+        hamburgerMenuState='close';
+    }
+}
+//------------code pour le carousel----------------
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+console.log('qeth');
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+let supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+let wheelOpt = supportsPassive ? { passive: false } : false;
+let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  setTimeout(enableScroll,1000);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
